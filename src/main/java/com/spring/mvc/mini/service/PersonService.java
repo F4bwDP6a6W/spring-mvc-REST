cffbb6f44da0f5129d84462e2215264027cbecdf
Jerry Zhang <hot13399@163.com>
2016-08-16 22:53:03 -0700

    database

---
+++ src/main/java/com/spring/mvc/mini/service/PersonService.java
@@ -0,0 +1,15 @@
+package com.journaldev.spring.service;
+
+import com.journaldev.spring.model.Person;
+
+import java.util.List;
+
+public interface PersonService {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+	
+}
---
+++ src/main/java/com/spring/mvc/mini/service/PersonServiceImpl.java
@@ -0,0 +1,49 @@
+package com.journaldev.spring.service;
+
+import com.journaldev.spring.dao.PersonDAO;
+import com.journaldev.spring.model.Person;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+	
+	private PersonDAO personDAO;
+
+	public void setPersonDAO(PersonDAO personDAO) {
+		this.personDAO = personDAO;
+	}
+
+	@Override
+	@Transactional
+	public void addPerson(Person p) {
+		this.personDAO.addPerson(p);
+	}
+
+	@Override
+	@Transactional
+	public void updatePerson(Person p) {
+		this.personDAO.updatePerson(p);
+	}
+
+	@Override
+	@Transactional
+	public List<Person> listPersons() {
+		return this.personDAO.listPersons();
+	}
+
+	@Override
+	@Transactional
+	public Person getPersonById(int id) {
+		return this.personDAO.getPersonById(id);
+	}
+
+	@Override
+	@Transactional
+	public void removePerson(int id) {
+		this.personDAO.removePerson(id);
+	}
+
+}
